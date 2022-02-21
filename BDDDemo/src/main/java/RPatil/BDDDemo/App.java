14b58208170ca9c871f24ded0db83925458c0cb5
Reshptl <reshma.m.patil@gmail.com>
2022-02-21 12:52:59 +0000

    Bdd Framework project initialization

---
+++ BDDDemo/src/main/java/RPatil/BDDDemo/App.java
@@ -0,0 +1,13 @@
+package RPatil.BDDDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
